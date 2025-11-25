efc377472fdd37723702b75f8fa7ce62905d91fc
awatdsu <okhinav@gmail.com>
2025-11-25 14:01:51 +0300

    .

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ import jwt
 import requests
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DB_FILE = os.path.join(BASE_DIR,"taskstate", "data", "taskstate.db")
-
-sys.path.insert(1, BASE_DIR)
+DB_FILE = "/root/taskstate/data/taskstate.db"
+sys.path.insert(1, "/root/taskstate")
 
 from tsapp.sconfig import JWT_KEY
 
